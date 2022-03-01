277f85af3194164ca481cd91c7e3d40cf0a03107
Li Li <ailei.aragon@gmail.com>
2022-03-01 08:15:18 -0500

    Update beta.py

--- beta.py
+++ beta.py
@@ -3,7 +3,7 @@ import pandas as pd  # pip install pandas openpyxl
 import streamlit as st  # pip install streamlit
 import requests
 import json
-from xls2json import build_import
+# from xls2json import build_import
 
 # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="API BETA PLAYGROUND",
@@ -12,6 +12,57 @@ st.set_page_config(page_title="API BETA PLAYGROUND",
 st.sidebar.header("Please Put Endpoint Here:")
 endpoint_selected = st.sidebar.text_input("Endpoints: ", 'regions')
 
+def build_dict(data_source, row):
+    column = list(data_source.columns)
+    dict_temp = {}
+    for i in column:
+        if '.' in i:
+            key_parent = i[0:i.find('.')]
+            key_child = i[i.find('.')+1:len(i)]
+            if key_parent in dict_temp.keys():
+                dict_temp[f'{key_parent}'].update(
+                    {f'{key_child}': f"{data_source[f'{i}'][row]}"})
+            else:
+                dict_temp.update(
+                    {f'{key_parent}': {f'{key_child}': f"{data_source[f'{i}'][row]}"}})
+        else:
+            dict_temp.update({f"{i}": f"{data_source[f'{i}'][row]}"})
+
+    return dict_temp
+
+
+def build_lookup(lookuplist, data_source, row):
+    lookup_temp = {}
+    for i in lookuplist:
+        lookup_temp.update({f"{i}": f"{data_source[f'{i}'][row]}"})
+    return lookup_temp
+
+
+def build_import(template_file):
+    # from .import build_dict
+    source_to_import = {"onFailure": "ABORT", "operations": []}
+    endpoint_list = pd.ExcelFile(template_file).sheet_names
+    api_objects = pd.read_csv(
+        'api_objects.csv', dtype=str)
+    for endpoint in endpoint_list:
+        data_source = pd.read_excel(
+            io=template_file,
+            engine="xlrd",
+            sheet_name=f'{endpoint}',
+            dtype=str
+        )
+        lookup = api_objects.loc[api_objects['endpoint']
+                                 == endpoint, 'lookup'].item()
+        lookup_list = lookup.split(",")
+        data_source.fillna('', inplace=True)
+        total_source = len(data_source[data_source.columns[0]])
+        for i in range(total_source):
+            data_temp = build_dict(data_source, i)
+            lookup_temp = build_lookup(lookup_list, data_source, i)
+            source_to_import['operations'].append({'lookup': lookup_temp,
+                                                   'action': 'REPLACE', 'resource': f'{endpoint}',
+                                                   'data': data_temp})
+    return source_to_import
 
 def get_token(url, access):
 
@@ -50,7 +101,6 @@ user_pwd = st.file_uploader(
     "Please put your pwd")
 
 if user_pwd is not None:
-  
   token = get_token(
     "https://dataservicetracktik.guards.app/rest/v1/auth", user_pwd)
   payload = {}
@@ -58,12 +108,9 @@ if user_pwd is not None:
       'Authorization': 'Bearer '+token,
       'Cookie': 'PHPSESSID=vqru8j08ho4oe1uaht3d6mikchqak2or'
   }
-
   response = requests.request(
       "GET", "https://dataservicetracktik.guards.app/rest/v1/batch/file?" + param_path, headers=headers, data=payload)
-
   data = response.json()
-
   df = pd.json_normalize(data=data['operations']).filter(regex='^data.')
   df.columns = df.columns.str.replace("data.", "", regex=True)
   st.dataframe(df)
