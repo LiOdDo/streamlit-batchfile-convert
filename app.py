3abd2a094cca4088c82137c746924819d564009b
Li Li <ailei.aragon@gmail.com>
2025-09-08 09:16:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,15 +9,7 @@ st.set_page_config(
 import subprocess
 from playwright.sync_api import sync_playwright
 from playwright.async_api import async_playwright
-subprocess.run(["playwright", "install", "--with-deps", "chromium"], check=True)
 
-# Then launch browser
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=True)
-    page = browser.new_page()
-    page.goto("https://example.com")
-    print(page.title())
-    browser.close()
 
 import pandas as pd  # pip install pandas openpyxl
 # import plotly.express as px  # pip install plotly-express
@@ -612,6 +604,7 @@ if services_selected == "TQL-Multi-Reports-Pivot Service":
             #         token, url_input, report_metric_file)
 
     # ---SIDEBAR---
+
 
 
 
